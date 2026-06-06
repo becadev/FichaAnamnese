593fef7fd198538fcd2234cc745170d63dca8bab
becadev <rebecanoemi494@gmail.com>
2026-06-05 22:33:32 -0300

    config banco

--- FichaAnamnese_api/app/main.py
+++ FichaAnamnese_api/app/main.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+from app.database import Base, engine
+
+Base.metadata.create_all(bind=engine)
+
 @app.get("/")
 def raiz():
     return {"mensagem": "API rodando!"}
